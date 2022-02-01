0b296a79401230d515782b6e2fa08bce98fc0355
Maxime Daniel <root@maxux.net>
2022-02-01 13:11:25 +0100

    tools: compare local network with upstream chain

--- tools/blocks.py
+++ tools/blocks.py
@@ -1,29 +1,89 @@
 import requests
+import json
 
-# classic rpc call
-targets = [
-    {"name": "Harmony", "host": "127.0.0.1:9500", "request": "hmy_blockNumber"},
-    {"name": "Fuse",    "host": "127.0.0.1:8545", "request": "eth_blockNumber"},
-]
+class RPC:
+    def __init__(self):
+        pass
 
-for target in targets:
-    rpc = {"jsonrpc": "2.0", "method": target["request"], "params":[], "id":1}
-    r = requests.post("http://%s" % target["host"], json=rpc)
-    data = r.json()
+    def call(self, endpoint, method):
+        rpc = {"jsonrpc": "2.0", "method": method, "params": [], "id": 1}
+        r = requests.post(endpoint, json=rpc)
+        data = r.json()
 
-    print("%-20s: block %d" % (target["name"], int(data["result"], 16)))
+        return data["result"]
 
 
-# pokt cal
+class NetworkStatus:
+    def __init__(self):
+        self.rpc = RPC()
 
-# http://localhost:8081/v1/query/height
-targets = [
-    {"name": "Pokt", "host": "127.0.0.1:8081", "url": "/v1/query/height"},
-]
+    def pokt(self):
 
-for target in targets:
-    r = requests.post("http://%s/%s" % (target["host"], target["url"]))
-    data = r.json()
+        gql = {
+            "operationName": "highestBlock",
+            "variables":{},
+            "query": "query highestBlock {\n  highestBlock {\n    item {\n      height\n      time\n       __typename\n    }\n    validatorThreshold\n    __typename\n  }\n}\n"
+        }
 
-    print("%-20s: height %d" % (target["name"], data["height"]))
+        r = requests.post("https://poktscan.com/api/graphql?opname=getPoktNetworkStatus", json=gql)
+        pokt = r.json()
+
+        return pokt["data"]["highestBlock"]["item"]["height"]
+
+    def fuse(self):
+        number = self.rpc.call("https://explorer.fuse.io/api/eth-rpc", "eth_blockNumber")
+        return int(number, 16)
+
+    def harmony(self):
+        number = self.rpc.call("https://api.s0.t.hmny.io", "hmy_blockNumber")
+        return int(number, 16)
+
+
+class BlockchainsStatus:
+    def __init__(self):
+        self.rpc = RPC()
+
+    def pokt(self):
+        r = requests.post("%s/%s" % ("http://127.0.0.1:8081", "/v1/query/height"))
+        data = r.json()
+
+        return data["height"]
+
+    def harmony(self):
+        number = self.rpc.call("http://127.0.0.1:9500", "hmy_blockNumber")
+        return int(number, 16)
+
+    def fuse(self):
+        number = self.rpc.call("http://127.0.0.1:8545", "eth_blockNumber")
+        return int(number, 16)
+
+if __name__ == "__main__":
+    n = NetworkStatus()
+    b = BlockchainsStatus()
+
+    chains = {
+        "fuse": {"name": "Fuse", "now": 0, "network": 0},
+        "pokt": {"name": "Pokt", "now": 0, "network": 0},
+        "harm": {"name": "Harmony", "now": 0, "network": 0}
+    }
+
+    print("[+] fetching local nodes state")
+    chains["fuse"]["now"] = b.fuse()
+    chains["pokt"]["now"] = b.pokt()
+    chains["harm"]["now"] = b.harmony()
+
+    print("[+] fetching network state: fuse")
+    chains["fuse"]["network"] = n.fuse()
+
+    print("[+] fetching network state: pokt")
+    chains["pokt"]["network"] = n.pokt()
+
+    print("[+] fetching network state: harmony")
+    chains["harm"]["network"] = n.harmony()
+
+    print("[+] =================")
+
+    for name in chains:
+        chain = chains[name]
+        print("[+] %-15s: % 12d / % 12d [% 3.1f %%]" % (chain["name"], chain["now"], chain["network"], (chain["now"] / chain["network"]) * 100))
 
