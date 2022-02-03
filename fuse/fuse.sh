1acfb7cffbd9f5536fb68bebe09bf3b518678a7c
Maxime Daniel <root@maxux.net>
2022-02-03 13:42:32 +0100

    fuse: make fuse cleaner

--- fuse/fuse.sh
+++ fuse/fuse.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
+set -ex
 
+# root is where the root directory config and data will be on the host
+scriptdir=$(dirname $0)
 root="/mnt/storage/blockchain/fuse"
 
-docker run --rm -it --net host -v ${root}/data:/root/data -v ${root}/keystore:/root/keystore -v ${root}/config/fuse.toml:/root/config.toml --name fuse-000 fusenet/node:2.0.1 -p --no-warp --config=/root/config.toml
+if [ -d ${root} ]; then
+    rm -rf ${root}/keys
+    rm -rf ${root}/conf
+fi
+
+# ensure default directory layout
+mkdir -p ${root}/keys
+mkdir -p ${root}/data
+mkdir -p ${root}/conf
+
+# copy config file from template and run script
+cp ${scriptdir}/fuse.toml ${root}/conf/fuse.toml
+
+docker run --rm -it --name fuse-000 --net host \
+    -v ${root}/data:/root/data \
+    -v ${root}/keys:/root/keystore \
+    -v ${root}/conf/fuse.toml:/root/config.toml \
+    fusenet/node:2.0.1 -p --no-warp --config=/root/config.toml
